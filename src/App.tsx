c28dd6ff6282c970fb9638afc27b4d4a9a07da30
Juan Manuel Shambuetta <shjuanmsh@gmail.com>
2025-06-26 16:48:54 -0500

    Connecting to Firebase

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,18 @@
-import { useEffect, useState } from "react";
-import UserForm from "./components/UserForm";
-import UserList from "./components/UserList";
+import { useState } from "react";
+import { createItem } from "./services/Api";
 
-function App() {
-  const [users, setUsers] = useState([]);
-  const [editingUser, setEditingUser] = useState(null);
-
-  // Cargar desde localStorage al montar
-  useEffect(() => {
-    const storedUsers = localStorage.getItem("users");
-    if (storedUsers) {
-      setUsers(JSON.parse(storedUsers));
-    }
-  }, []);
-
-  // Guardar en localStorage cuando cambian los usuarios
-  useEffect(() => {
-    localStorage.setItem("users", JSON.stringify(users));
-  }, [users]);
-
-  const addUser = (user) => {
-    setUsers([...users, { ...user, id: crypto.randomUUID() }]);
-  };
-
-  const deleteUser = (id) => {
-    setUsers(users.filter((user) => user.id !== id));
-  };
-
-  const updateUser = (updatedUser) => {
-    setUsers(
-      users.map((user) => (user.id === updatedUser.id ? updatedUser : user))
-    );
-    setEditingUser(null);
-  };
-
-  const editUser = (user) => {
-    setEditingUser(user);
-  };
-
+const App = () => {
+  const [title, setTitle] = useState("");
   return (
-    <div style={{ padding: "20px" }}>
-      <h1>CRUD con React y LocalStorage</h1>
-      <UserForm
-        addUser={addUser}
-        editingUser={editingUser}
-        updateUser={updateUser}
+    <>
+      <input
+        type="text"
+        onChange={(e) => setTitle(e.target.value)}
+        placeholder="Title:"
       />
-      <UserList users={users} deleteUser={deleteUser} editUser={editUser} />
-    </div>
+      <button onClick={() => createItem({ title: title })}>Crear</button>
+    </>
   );
-}
+};
 
 export default App;
